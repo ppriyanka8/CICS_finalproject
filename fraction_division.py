22bb7024bbb5b3cc58a35446eff22c0a648488d0
ppriyanka <ppriyanka@umass.edu>
2024-09-18 16:17:57 -0400

    test commit

---
+++ fraction_division.py
@@ -0,0 +1,24 @@
+# Author: Priyanka
+# Email: ppriyanka@umass.edu
+# SPIRE ID: 34056915
+
+a=int(input("What is the dividend: "))
+b=int(input("What is the divisor: "))
+print(str(a) + "/" + str(b) + " equals:")
+result1=a//b
+result2=a%b
+print("\t" + str(result1) + " and " + str(result2) + "/" + str(b))
+"""
+74/42 equals: ' != '74/42 equals:'\n- 74/42 equals: \n? -\n+ 74/42 equals:\n : \u274c 
+output for inputs 74, 42 was incorrect for the first line of output\n
+        You can look at the comparison for more information, line with a minus is what you have, 
+the plus sign is what we have. \n\tA plus below an empty space means you need to add a 
+space there in your output.\n\tA minus below and empty space means you need to remove a space there 
+in your ouput\n",
+            "number": "9"
+"""
+
+
+
+
+
